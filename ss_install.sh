3bb4b7af02d67074718de2a0f38a090721e4bd8a
gaarayx <137686692@qq.com>
2018-08-12 01:27:52 -0700

    [ss] Add initial script.
    this script is simple and primitive, but it works.

---
+++ ss_install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+p=$1
+password=$2
+
+# install shadowsocks by pip
+apt-get update
+apt-get install python-pip
+apt-get install python-setuptools
+pip install shadowsocks
+
+# create config file
+echo "{\"server\":\"$1\", \"server_port\":8388, \"local_address\": \"127.0.0.1\", \"local_port\":1080, \"password\":\"$2\",	\"timeout\":300, \"method\":\"aes-256-cfb\"}" > /etc/shadowsocks.json
+
+# fix EVP_CIPHER_CTX_cleanup no found
+sed -i s/EVP_CIPHER_CTX_cleanup/EVP_CIPHER_CTX_reset/g /usr/local/lib/python2.7/dist-packages/shadowsocks/crypto/openssl.py
+
+# start service
+ssserver -c /etc/shadowsocks.json -d start
