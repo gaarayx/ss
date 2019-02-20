dc6ef362500771142bc3344b74953f07c72b41d7
gaarayx <137686692@qq.com>
2019-02-20 22:08:48 +0800

    [ss] Modify server_port and local_port

--- ss_install.sh
+++ ss_install.sh
@@ -11,7 +11,7 @@ pip install wheel
 pip install shadowsocks
 
 # create config file
-echo "{\"server\":\"$1\", \"server_port\":8388, \"local_address\": \"127.0.0.1\", \"local_port\":1080, \"password\":\"$2\",	\"timeout\":300, \"method\":\"aes-256-cfb\"}" > /etc/shadowsocks.json
+echo "{\"server\":\"$1\", \"server_port\":443, \"local_address\": \"127.0.0.1\", \"local_port\":2080, \"password\":\"$2\",	\"timeout\":300, \"method\":\"aes-256-cfb\"}" > /etc/shadowsocks.json
 
 # fix EVP_CIPHER_CTX_cleanup no found
 sed -i s/EVP_CIPHER_CTX_cleanup/EVP_CIPHER_CTX_reset/g /usr/local/lib/python2.7/dist-packages/shadowsocks/crypto/openssl.py
