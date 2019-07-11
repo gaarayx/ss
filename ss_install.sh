f39f943a28469a86464af470b69670b7441cc055
root <root@vultr.guest>
2019-07-11 15:07:40 +0000

    [ss] Get server ip automatically

--- ss_install.sh
+++ ss_install.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
 
-ip=$1
-password=$2
+net_dev=`ifconfig | head -n 1 | cut -d: -f 1`
+
+ip=`ifconfig $net_dev | grep netmask | awk '{print $2}'`
+
+if [ "$1" != "" ]; then
+	pw=$1
+else
+	pw="defaultpasswd"
+fi
+
+echo [DEBUG]:net_dev=$net_dev ip=$ip pw=$pw
 
 # install shadowsocks by pip
 apt-get update
-apt-get install python-pip
-apt-get install python-setuptools
+apt-get -y install python-pip
+apt-get -y install python-setuptools
 pip install wheel
 pip install shadowsocks
 
 # create config file
-echo "{\"server\":\"$1\", \"server_port\":443, \"local_address\": \"127.0.0.1\", \"local_port\":2080, \"password\":\"$2\",	\"timeout\":300, \"method\":\"aes-256-cfb\"}" > /etc/shadowsocks.json
+echo "{\"server\":\"$ip\", \"server_port\":443, \"local_address\": \"127.0.0.1\", \"local_port\":2080, \"password\":\"$pw\",	\"timeout\":300, \"method\":\"aes-256-cfb\"}" > /etc/shadowsocks.json
 
 # fix EVP_CIPHER_CTX_cleanup no found
 sed -i s/EVP_CIPHER_CTX_cleanup/EVP_CIPHER_CTX_reset/g /usr/local/lib/python2.7/dist-packages/shadowsocks/crypto/openssl.py
